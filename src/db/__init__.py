4e8ff92debd1473a46eb0c4d602450a7486b46cd
Pierre Legrand <pierrelegrand.pro@gmail.com>
2023-02-18 15:26:23 +0100

    [U] Refactor code.

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,1 +1,15 @@
+from abc import ABC
+from pathlib import Path
 
+from tinydb import TinyDB
+
+
+class Database(ABC):
+
+    def __init__(self, db_path: Path):
+        self._db_path = db_path
+        self._db = TinyDB(self._db_path, indent=2)
+
+    @property
+    def db(self):
+        return self._db
